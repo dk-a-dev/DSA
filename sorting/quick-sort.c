7aa599c5e714b337338a5eb4b667a79c3c54a76f
Dev Keshwani <dev.keshwani345@gmail.com>
2023-10-05 11:23:42 +0530

    fix: quick sort breaking changes

--- sorting/quick-sort.c
+++ sorting/quick-sort.c
@@ -1,3 +1,21 @@
+// Quick Sort or Partition Exchange Sort
+/*
+Quick Sort is a Divide and Conquer algorithm.
+It picks an element as pivot and partitions the given array around the picked pivot.
+There are many different versions of quickSort that pick pivot in different ways.
+1. Always pick first element as pivot. (implemented below)
+2. Always pick last element as pivot.
+3. Pick a random element as pivot.
+4. Pick median as pivot.
+*/
+// The key process in quickSort is partition().
+// Target of partitions is, given an array and an element x of array as pivot,
+// put x at its correct position in sorted array and put all smaller elements (smaller than x) before x,
+// and put all greater elements (greater than x) after x.
+// All this should be done in linear time.
+// Time Complexity: O(n^2) in worst case and O(nlogn) in average case.
+// Space Complexity: O(1)
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,6 +24,14 @@ void input(int *arr, int n)
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 }
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = *a;
+}
+
 void quick_sort(int *arr, int low, int high)
 {
     if (low < high)
@@ -23,11 +49,7 @@ void quick_sort(int *arr, int low, int high)
                 j--;
             // swap the elements
             if (i < j)
-            {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+                swap(&arr[i], &arr[j]);
         }
         // swap the pivot element
         arr[low] = arr[j];
