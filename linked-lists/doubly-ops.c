c26011d242835ab556ec440ab4f5714858e6842a
Dev Keshwani <dev.keshwani345@gmail.com>
2023-10-10 09:21:27 +0530

    refactor: double linked lists

--- linked-lists/doubly-ops.c
+++ linked-lists/doubly-ops.c
@@ -1,175 +1,87 @@
-// Doubly linked list and its operations. (2 marks)
-
-// a)     Creation of linked list with numbers 2 9 10 5 6 3 1 and display the contents of the doubly linked list
-
-// b)     Adding a number 11 to the end and display the contents of the doubly linked list
-
-// c)     Adding a number 12 in the beginning and displaying the contents of the doubly linked list
-
-// d)     Adding a number 14 in position 4 and displaying the contents of the doubly linked list
-
-// e)     Searching a number 10 and displaying its position in the doubly linked list
-
-// f)      Deleting a number 9 from the list and display the contents of the doubly linked list
+// Doubly linked list and its operations
+// Creation
+// Insertion
+// Deletion
+// Displaying the contents of the list
+// Reverse the list
 
 #include <stdio.h>
 #include <stdlib.h>
 
 struct node
 {
-    int data;
-    struct node *next;
-    struct node *prev;
+    int data;          // current node data
+    struct node *next; // pointer to next node(address of next node)
+    struct node *prev; // pointer to previous node(address of previous node)
 };
 
-struct node *head = NULL;
+struct node *head = NULL; // pointer to head node(address of head node)
 
-void create()
-{
-    struct node *newnode, *temp;
-    int n=7, i;
-    for (i = 0; i < n; i++)
-    {
-        newnode = (struct node *)malloc(sizeof(struct node));
-        scanf("%d", &newnode->data);
-        newnode->next = NULL;
-        newnode->prev = NULL;
-        if (head == NULL)
-        {
-            head = newnode;
-            temp = newnode;
-        }
-        else
-        {
-            temp->next = newnode;
-            newnode->prev = temp;
-            temp = newnode;
-        }
-    }
-}
-
-void display()
-{
-    struct node *temp;
-    temp = head;
-    while (temp != NULL)
-    {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-void insert_end()
+void creator()
 {
     struct node *newnode, *temp;
     newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data=11;
+    printf("Enter the data for node:");
+    scanf("%d", &newnode->data); // or *(newnode).data
     newnode->next = NULL;
     newnode->prev = NULL;
-    temp = head;
-    while (temp->next != NULL)
+    if (head == NULL)
     {
-        temp = temp->next;
+        head = temp = newnode;
     }
-    temp->next = newnode;
-    newnode->prev = temp;
-}
-
-void insert_beg()
-{
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data=12;
-    newnode->next = NULL;
-    newnode->prev = NULL;
-    newnode->next = head;
-    head->prev = newnode;
-    head = newnode;
-}
-
-void insert_pos()
-{
-    struct node *newnode, *temp;
-    int pos=3, i;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data=14;
-    newnode->next = NULL;
-    newnode->prev = NULL;
-    temp = head;
-    for (i = 0; i < pos - 1; i++)
+    else
     {
-        temp = temp->next;
+        temp->next = newnode;
+        newnode->prev = temp;
+        temp = newnode;
     }
-    newnode->next = temp->next;
-    temp->next->prev = newnode;
-    temp->next = newnode;
-    newnode->prev = temp;
 }
 
-void search()
+int main(int argc, char const *argv[])
 {
-    struct node *temp;
-    int key=10, pos = 0;
-    temp = head;
-    while (temp != NULL)
+    int choice;
+    while (1)
     {
-        pos++;
-        if (temp->data == key)
+        printf("\n\n1. Create a doubly linked list\n");
+        printf("2. Insert a node at the beginning\n");
+        printf("3. Insert a node at the end\n");
+        printf("4. Insert a node at a given position\n");
+        printf("5. Delete a node from a given position\n");
+        printf("6. Display the contents of the list\n");
+        printf("7. Reverse the list\n");
+        printf("8. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
         {
-            printf("Item 10 Found in position:%d\n",pos);
-            return;
+        case 1:
+            creator();
+            break;
+        case 2:
+            insert_begin();
+            break;
+        case 3:
+            insert_end();
+            break;
+        case 4:
+            insert_pos();
+            break;
+        case 5:
+            delete ();
+            break;
+        case 6:
+            display();
+            break;
+        case 7:
+            reverse();
+            break;
+        case 8:
+            exit(0);
+        default:
+            printf("Invalid choice!\n");
+            break;
         }
-        temp = temp->next;
     }
-}
 
-void delete(int key)
-{
-    struct node *temp;
-    temp = head;
-    while (temp != NULL)
-    {
-        if (temp->data == key)
-        {
-            if (temp == head)
-            {
-                head = temp->next;
-                temp->next->prev = NULL;
-                free(temp);
-                return;
-            }
-            else if (temp->next == NULL)
-            {
-                temp->prev->next = NULL;
-                free(temp);
-                return;
-            }
-            else
-            {
-                temp->prev->next = temp->next;
-                temp->next->prev = temp->prev;
-                free(temp);
-                return;
-            }
-        }
-        temp = temp->next;
-    }
-}
-
-int main()
-{
-    create();
-    display();
-    insert_end();
-    display();
-    delete(11);
-    insert_beg();
-    display();
-    insert_pos();
-    display();
-    search();
-    delete(9);
-    display();
     return 0;
 }
