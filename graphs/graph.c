339b967c0c92605476351e59b64025670d20a0f0
Dev Keshwani <dev.keshwani345@gmail.com>
2023-10-30 12:29:07 +0530

    feat: graph traversal and creation

---
+++ graphs/graph.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void input(int **graph, int vertices)
+{
+    for (int i = 0; i < vertices; i++)
+        graph[i] = (int *)malloc(vertices * sizeof(int));
+    for (int i = 0; i < vertices; i++)
+        for (int j = 0; j < vertices; j++)
+            scanf("%d", &graph[i][j]);
+}
+
+void bfs(int **graph, int vertices, int start)
+{
+    int *visited=(int*)calloc(vertices,sizeof(int));
+    int *queue=(int*)calloc(vertices,sizeof(int));
+    int front=0, rear=0;
+    queue[rear++]=start;
+    visited[start]=1;
+    while(front!=rear)
+    {
+        int node=queue[front++];
+        for(int i=0;i<vertices;i++)
+        {
+            if(graph[node][i]==1 && visited[i]==0)
+            {
+                queue[rear++]=i;
+                visited[i]=i+1;
+            }
+        }
+    }
+    for(int i=0;i<vertices;i++)
+        if(i==vertices-1)
+            printf("%d",queue[i]+1);
+        else
+            printf("%d->",queue[i]+1);
+}
+
+void dfs(int **graph, int vertices, int start)
+{
+    int *visited=(int*)calloc(vertices,sizeof(int));
+    int *stack=(int*)calloc(vertices,sizeof(int));
+    int *temp=(int*)calloc(vertices,sizeof(int));
+    int index=0;
+    int top=-1;
+    stack[++top]=start;
+    visited[start]=1;
+    while(top!=-1)
+    {
+        int node=stack[top--];
+        temp[index++]=node+1;
+        for(int i=0;i<vertices;i++)
+        {
+            if(graph[node][i]==1 && visited[i]==0)
+            {
+                stack[++top]=i;
+                visited[i]=i+1;
+            }
+        }
+    }
+    for(int i=0;i<vertices;i++)
+        if(i==vertices-1)
+            printf("%d",temp[i]);
+        else
+            printf("%d->",temp[i]);
+}
+
+int main(int argc, char const *argv[])
+{
+    int vertices;
+    scanf("%d", &vertices);
+    int **graph = (int **)malloc(vertices * sizeof(int *));
+    input(graph, vertices);
+    bfs(graph, vertices, 0);
+    printf("\n");
+    dfs(graph, vertices, 0);
+    return 0;
+}
