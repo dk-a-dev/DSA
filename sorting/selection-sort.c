3582985096ca2713ad19c5b87130f4792b335f48
Dev Keshwani <dev.keshwani345@gmail.com>
2023-12-01 21:30:59 +0530

    fix: improved optimization

--- sorting/selection-sort.c
+++ sorting/selection-sort.c
@@ -22,7 +22,7 @@ void selectionSort(int *arr, int size)
     for (int i = 0; i < size - 1; i++)
     {
         min = i;
-        for (int j = i; j < size; j++)
+        for (int j = i+1; j < size; j++)
         {
             if (arr[j] < arr[min])
                 min = j;
