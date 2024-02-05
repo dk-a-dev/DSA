48f0f48e2457a90002996f32426a01435383c120
Dev Keshwani <dev.keshwani345@gmail.com>
2024-02-05 19:36:28 +0530

    feat: rewrite frac-knapsack

--- DAA/fractional-knapsack-algo.c
+++ DAA/fractional-knapsack-algo.c
@@ -1,78 +1,88 @@
-// fractional knapsack algorithm uing greedy method
-// Time Complexity: O(nlogn)
-
 #include <stdio.h>
 #include <stdlib.h>
 
-struct item
+int main()
 {
-    int weight;
-    int profit;
-    float ratio;
-    float fraction;
-};
-
-void swap(struct item *a, struct item *b)
-{
-    struct item temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-float fractionalKnapsack(struct item *arr, int n, int W)
-{
-
-    float profit = 0;
-    float weight = 0;
-    for (int i = 0; i < n; i++)
+    int n, i, j;
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+    double *profit = (double *)malloc(n * sizeof(double));
+    double *weight = (double *)malloc(n * sizeof(double));
+    double *ratio = (double *)malloc(n * sizeof(double));
+    printf("Enter the profit and weight of the items: \n");
+    for (i = 0; i < n; i++)
     {
-        // sorting
-        int max = i;
-        for (int j = i + 1; j < n; j++)
+        scanf("%lf %lf", &profit[i], &weight[i]);
+        ratio[i] = profit[i] / weight[i];
+    }
+    double capacity;
+    printf("Enter the capacity of the knapsack: ");
+    scanf("%lf", &capacity);
+    double total_profit = 0;
+    double *x = (double *)malloc(n * sizeof(double));
+    for (i = 0; i < n; i++)
+    {
+        x[i] = 0;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
         {
-            if (arr[j].ratio > arr[max].ratio)
+            if (ratio[i] < ratio[j])
             {
-                max = j;
+                double temp = ratio[i];
+                ratio[i] = ratio[j];
+                ratio[j] = temp;
+                temp = profit[i];
+                profit[i] = profit[j];
+                profit[j] = temp;
+                temp = weight[i];
+                weight[i] = weight[j];
+                weight[j] = temp;
             }
         }
-        swap(&arr[i], &arr[max]);
-        // end of sorting
-
-        // calculating profit
-        if (weight + arr[i].weight <= W)
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (weight[i] > capacity)
         {
-            weight += arr[i].weight;
-            profit += arr[i].profit;
-            arr[i].fraction = 1;
+            break;
         }
         else
         {
-            int remain = W - weight;
-            profit += arr[i].ratio * remain;
-            weight += remain;
-            arr[i].fraction = (float)remain / arr[i].weight;
+            x[i] = 1;
+            total_profit += profit[i];
+            capacity -= weight[i];
         }
-
-        printf("\n");
-        printf("%d %d %d %f %f\n", i + 1, arr[i].profit, arr[i].weight, arr[i].ratio, arr[i].fraction);
     }
-    return profit;
-}
-
-int main()
-{
-    int n, W;
-    printf("Enter the number of items: ");
-    scanf("%d", &n);
-    struct item *arr = (struct item *)malloc(n * sizeof(struct item));
-    printf("Enter the profit and weight of each item:\n");
-    for (int i = 0; i < n; i++)
+    if (i < n)
     {
-        scanf("%d%d", &arr[i].profit, &arr[i].weight);
-        arr[i].ratio = (float)arr[i].profit / arr[i].weight;
+        x[i] = capacity / weight[i];
+        total_profit += x[i] * profit[i];
     }
-    printf("Enter the capacity of the knapsack: ");
-    scanf("%d", &W);
-    printf("Maximum profit: %.2f\n", fractionalKnapsack(arr, n, W));
+
+    printf("Set of items in knapsack: {");
+    for (i = 0; i < n; i++)
+    {
+        printf("%c", 65 + i);
+        if (i < n - 1)
+        {
+            printf(", ");
+        }
+    }
+
+    printf("}=[");
+    for (i = 0; i < n; i++)
+    {
+        printf("%lf", x[i]);
+        if (i < n - 1)
+        {
+            printf(", ");
+        }
+    }
+    
+    printf("]\n");
+
+    printf("Total value of items taken into knapsack: %lf\n", total_profit);
     return 0;
 }
